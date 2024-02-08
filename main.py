131af4498daaea07dbf16f34d7482d8a224019eb
protein <taimanova.sveta@gmail.com>
2024-02-08 16:23:09 +0300

    Сделала нормальный импорт

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import pygame
 import pytmx
 from level import Fireboy_and_Watergirl
-from const import WINDOW_SIZE, WINDOW_WIDTH, WINDOW_HEIGHT, FPS, MAPS_DIR, TILE_SIZE
-
+from characters import Fireboy, Watergirl
+from const import *
 
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode(WINDOW_SIZE)
     fb_wg = Fireboy_and_Watergirl("map1.tmx", [10, 46], 20)
-
     clock = pygame.time.Clock()
     running = True
     while running:
