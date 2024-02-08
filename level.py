cba0566a63db76bc784d5615da8d11eb9fa60db0
miillaa <liudmila.lisina01@yandex.ru>
2024-02-08 16:05:50 +0300

    Карты уровней, файл main

---
+++ level.py
@@ -0,0 +1,26 @@
+import pygame
+import pytmx
+
+WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 640, 640
+FPS = 15
+MAPS_DIR = "maps"
+TILE_SIZE = 32
+
+
+class Fireboy_and_Watergirl:
+    def __init__(self, filename, free_tiles, finish_tile):
+        self.map = pytmx.load_pygame(f"{MAPS_DIR}/{filename}")
+        self.height = self.map.height
+        self.width = self.map.width
+        self.tile_size = self.map.tilewidth
+        self.free_tiles = free_tiles
+        self.finish_tile = finish_tile
+
+    def render(self, screen):
+        for y in range(self.height):
+            for x in range(self.width):
+                image = self.map.get_tile_image(x, y, 0)
+                screen.blit(image, (x * self.tile_size, y * self.tile_size))
+
+    def get_tile_id(self, position):
+        return self.map.tiledgidmap[self.map.get_tile_gid(*position, 0)]
